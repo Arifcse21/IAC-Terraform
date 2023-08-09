046bb7d7867affc792ec17a1ae33051cda456f65
ArifTF <arif.techforing@gmail.com>
2023-08-09 16:55:14 +0600

    deployment of mongodb in docker

---
+++ MongoDB/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+
+resource "docker_container" "mongo" {
+    name = "monga"
+    image = "mongo:latest"
+
+    ports {
+        internal = 27017
+        external = 27017
+    }
+  
+}
