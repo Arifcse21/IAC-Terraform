d2b558efc111345f23273341f7fd79ade070411b
ArifTF <arif.techforing@gmail.com>
2023-08-09 15:52:29 +0600

    elasticsearch deploy on docker

--- ElasticSearch/main.tf
+++ ElasticSearch/main.tf
@@ -1,15 +1,31 @@
 terraform {
-    required_provider {
-        docker = {
-            source  = "kreuzwerker/docker"
-            version = "3.0.2"
-        }
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
     }
+  }
 }
 
 provider "docker" {
-    host = "unix:///var/run/docker.sock"
+  host = "unix:///var/run/docker.sock"
+}
+
+
+# data.tf
+data "docker_image" "elasticsearch" {
+  name = "docker.elastic.co/elasticsearch/elasticsearch:7.14.0"
 }
 
 # Create the container
+resource "docker_container" "elasticsearch" {
+  name  = "elasticsearch"
+  image = data.docker_image.elasticsearch
 
+  ports {
+    internal = 9200
+    external = 9200
+  }
+
+  restart = "unless-stopped"
+}
